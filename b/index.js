7cccbf12e1fb6da7eb44bb1a7947baecbcdfe8fa
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-09-12 00:18:26 +0700

    Deploy app b

---
+++ b/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello app B!');
+});
+
+const port = process.env.PORT || 3000; // run web service on Heroku
+app.listen(port, function() {
+  console.log('Example app listening on port 3000!');
+});
